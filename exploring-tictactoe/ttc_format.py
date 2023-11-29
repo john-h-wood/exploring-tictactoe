5cd3d839f7d7c6f50689786c41cbe451edcc9f91
John Wood <23jwood@gmail.com>
2023-11-28 23:51:21 -0500

    Added function to print a flat board

--- exploring-tictactoe/ttc_format.py
+++ exploring-tictactoe/ttc_format.py
@@ -6,3 +6,14 @@ X | X | X
 ---------
 X | X | X
 """
+
+"""
+0   1   2
+3   4   5
+6   7   8
+"""
+
+
+def print_board(board):
+    for row in range(0, 9, 3):
+        print(board[row], board[row + 1], board[row + 2])
