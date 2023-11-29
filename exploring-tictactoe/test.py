7a25fd215f7311e830e1d2f7845560679f7eea5f
John Wood <23jwood@gmail.com>
2023-11-28 23:49:44 -0500

    Added test for recursively populating root state with state map

--- exploring-tictactoe/test.py
+++ exploring-tictactoe/test.py
@@ -1,8 +1,16 @@
 from ttc_theory import flatten_board
 from ttc_state import TTCState
-from ttc_constants import GAME_TURN_X
+from ttc_constants import GAME_TURN_X, GAME_WIN_NONE
 
-board = flatten_board([[2, 1, 2], [0, 0, 1], [1, 2, 1]])
+root_board = flatten_board([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+root_state = TTCState(root_board, GAME_TURN_X)
 
-board_state = TTCState(board, GAME_TURN_X)
-print(board_state.id, board_state.win_state)
+state_map = {root_state.id: root_state}
+
+root_state.recursively_populate_children(state_map)
+print(len(state_map))
+
+def print_random_game(state_map, root_state):
+    working_state = root_state
+    while working_state.win_state != GAME_WIN_NONE:
+
