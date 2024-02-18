6721be7df07a26af26b0378817f18683757a2fa1
John Wood <113411200+john-h-wood@users.noreply.github.com>
2024-02-17 19:48:23 -0500

    Added possibility descriptions and integer values.

--- exploring-tictactoe/ttc_constants.py
+++ exploring-tictactoe/ttc_constants.py
@@ -15,4 +15,29 @@ WIN_NONE = 0
 TURN_X = 1
 TURN_O = 2
 
+# POSSIBILITIES
+POSS_DESCRIPTIONS = {
+    'POSS_G_TIE': 'All child states have POSS_G_TIE and if they don\'t have children, '
+                  'they have WIN_TIE',
+    'POSS_G_X_WIN': 'All child states have POSS_G_X_WIN and if they don\'t have children, '
+                    'they have WIN_X',
+    'POSS_G_O_WIN': 'All child states have POSS_G_O_WIN and if they don\'t have children, '
+                    'they have WIN_O',
+    'POSS_C_X_WIN': 'Given specific child selections on states with TURN_X,'
+                    'there exists a path from this state to one with WIN_X',
+    'POSS_C_O_WIN': 'Given specific child selections on states with TURN_O,'
+                    'there exists a path from this state to one with WIN_O',
+    'POSS_C_X_TIE': 'Given specific child selections on states with TURN_X,'
+                    'there exists a path from this state to one with WIN_TIE',
+    'POSS_C_O_TIE': 'Given specific child selections on states with TURN_O,'
+                    'there exists a path from this state to one with WIN_TIE'
+}
 
+POSS_G_TIE = 0
+POSS_G_X_WIN = 1
+POSS_G_O_WIN = 2
+
+POSS_C_X_WIN = 3
+POSS_C_O_WIN = 4
+POSS_C_X_TIE = 5
+POSS_C_O_TIE = 6
